cb170a9d5f3affc69fd9dec1ce00749ee7065b8a
Rodrigo Filomeno <rodrigofilomeno@mac-mini-de-rodrigo.local>
2018-01-05 17:15:56 -0200

    Initial Commit

---
+++ MeuTreino/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeuTreino
+//
+//  Created by Rodrigo Filomeno on 05/01/2018.
+//  Copyright © 2018 Rodrigo Filomeno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
